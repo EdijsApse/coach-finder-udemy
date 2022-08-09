7167c61e12bd1435e7cd3e098acddb2829008fc0
Edijs <edijsapse@gmail.com>
2022-08-09 14:47:49 +0300

    Coach detail page

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -1,5 +1,53 @@
 <template>
-  DETAILS FOR A COACH
-  <router-link to="/coaches/id/contact">Contact</router-link>
-  <router-view></router-view>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>{{ rate }}</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now.</h2>
+        <base-button :link="true" :to="contactLink">Contact</base-button>
+      </header>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge v-for="area in areas" :key="area" :title="area" :type="area"></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
+
+<script>
+  export default {
+    props: ['id'],
+    data() {
+      return {
+        selectedCoach: null
+      }
+    },
+    computed: {
+      fullName() {
+        return `${this.selectedCoach.firstName} ${this.selectedCoach.firstName}` 
+      },
+      contactLink() {
+        return `${this.$route.path}/contact`
+      },
+      description() {
+        return this.selectedCoach.description
+      },
+      rate() {
+        return `$${this.selectedCoach.hourlyRate}/hour`;
+      },
+      areas() {
+        return this.selectedCoach.areas;
+      }
+    },
+    created() {
+      this.selectedCoach = this.$store.getters['coachesModule/coaches'].find((coach) => coach.id === this.id);
+    }
+  }
+</script>
