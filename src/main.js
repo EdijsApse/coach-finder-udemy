380eadb2f921588a432df9784e9b8509eef44b4e
Edijs <edijsapse@gmail.com>
2022-08-09 10:59:15 +0300

    Base UI components

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import { createApp } from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
+import BaseCard from './components/ui/BaseCard';
+import BaseButton from './components/ui/BaseButton'
+import BaseBadge from './components/ui/BaseBadge'
 
 const app = createApp(App);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
 
 app.use(router);
 app.use(store);
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,14 +3,16 @@
     FILTER
   </section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as a coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item v-for="coach in filteredCoaches" :key="coach.id" v-bind="coach"></coach-item>
-    </ul>
-    <h3 v-else>No coaches found</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button :link="true" to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item v-for="coach in filteredCoaches" :key="coach.id" v-bind="coach"></coach-item>
+      </ul>
+      <h3 v-else>No coaches found</h3>
+    </base-card>
   </section>
 </template>
 
